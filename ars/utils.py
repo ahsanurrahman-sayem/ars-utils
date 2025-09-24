4abb16ae82541892174afffa4d38b761d1d278d7
ahsanurrahman-sayem <skgroup10000@gmail.com>
2025-09-24 16:19:09 +0000

    New Functions and helper methods I used in a project has been added.

--- ars/utils.py
+++ ars/utils.py
@@ -42,3 +42,18 @@ def clear_reports():
 		print(f"{REPORT_DIR} has been cleared successfully.")
 	except Exception as e:
 		print(f"Error: {e}")
+
+def isZero( value: str):
+#this method checkhs if the given value is 0?
+#if its a 0 then return empty else returns timeStamp from the utils module, I Used this method in ScaleReport Application.
+	return "" if value == "0" else getNow()
+
+def isDigit(value):
+	#this method checks if the value is a number, if number then returns the value else retuns a 0.  I Used this method in ScaleReport Application.
+	return value if value.isdigit() else 0
+
+
+def isValue(value,valueToUse):
+# first check if value is not empty
+# if the value is empty then valueToUse otherwise return value. 
+	return value if value is not ""  else valueToUse
